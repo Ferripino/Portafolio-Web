0b2ff01b4de73876ed89de4c5d301d726da279da
Ferripino <141149188+Ferripino@users.noreply.github.com>
2025-01-18 22:14:44 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
 
@@ -8,10 +10,19 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     })
     .then(response => response.text())
     .then(data => {
-        alert(data); // Show server response
+        Swal.fire({
+            title: "Gracias por su mensaje",
+            text: "En un rato me pondré en contacto con usted",
+            icon: "success"
+          }); // Show server response
         this.reset(); // Reset the form
     })
     .catch(error => {
+        Swal.fire({
+            icon: "error",
+            title: "ERROR",
+            text: "Halgo a salido mal, por favor, intente de nuevo",
+          });
         console.error('Error:', error);
     });
 });
